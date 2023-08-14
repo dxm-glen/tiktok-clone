a9784413f0f3710e70b4af20e964e708cf872bd9
이정훈 <glen@ijeonghun-ui-MacBookAir.local>
2023-08-14 13:13:48 +0900

    finish birthday screen

---
+++ lib/features/authentication/login_form_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginFromScreen extends StatelessWidget {
+  const LoginFromScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
