2aa60b05eebad1743c327022151bcbaf4e6e8ae5
이정훈 <glen@ijeonghun-ui-MacBookAir.local>
2023-08-14 16:02:57 +0900

    add onScaffoldTap for unfocused

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -19,78 +19,84 @@ class _LoginFromScreenState extends State<LoginFromScreen> {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        print(formData);
       }
     }
   }
 
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size16,
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Login"),
         ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Gaps.v28,
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Email',
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size16,
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Gaps.v28,
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email is required.';
+                    }
+
+                    // 이메일 정규식 검증
+                    String pattern = r'^[^@\s]+@[^@\s]+\.[^@\s]+$';
+                    RegExp regex = RegExp(pattern);
+                    if (!regex.hasMatch(value)) {
+                      return 'Enter a valid email address.';
+                    }
+
+                    return null; // 올바른 입력인 경우
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData["email"] = newValue;
+                    }
+                  },
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Email is required.';
-                  }
+                Gaps.v16,
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Password is required.';
+                    }
 
-                  // 이메일 정규식 검증
-                  String pattern = r'^[^@\s]+@[^@\s]+\.[^@\s]+$';
-                  RegExp regex = RegExp(pattern);
-                  if (!regex.hasMatch(value)) {
-                    return 'Enter a valid email address.';
-                  }
+                    // 비밀번호 길이 검증
+                    if (value.length < 8) {
+                      return 'Password must be at least 8 characters long.';
+                    }
 
-                  return null; // 올바른 입력인 경우
-                },
-                onSaved: (newValue) {
-                  if (newValue != null) {
-                    formData["email"] = newValue;
-                  }
-                },
-              ),
-              Gaps.v16,
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Password',
+                    return null; // 올바른 입력인 경우
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData["password"] = newValue;
+                    }
+                  },
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Password is required.';
-                  }
-
-                  // 비밀번호 길이 검증
-                  if (value.length < 8) {
-                    return 'Password must be at least 8 characters long.';
-                  }
-
-                  return null; // 올바른 입력인 경우
-                },
-                onSaved: (newValue) {
-                  if (newValue != null) {
-                    formData["password"] = newValue;
-                  }
-                },
-              ),
-              Gaps.v28,
-              GestureDetector(
-                  onTap: _onSubmitTap,
-                  child: const FormButton(disabled: false, text: "Next"))
-            ],
+                Gaps.v28,
+                GestureDetector(
+                    onTap: _onSubmitTap,
+                    child: const FormButton(disabled: false, text: "Next"))
+              ],
+            ),
           ),
         ),
       ),
