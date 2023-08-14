0255d6a67ab3f5900726a20600cd86edc19ae258
이정훈 <glen@ijeonghun-ui-MacBookAir.local>
2023-08-14 15:53:18 +0900

    finish auth UI

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -13,10 +13,13 @@ class LoginFromScreen extends StatefulWidget {
 class _LoginFromScreenState extends State<LoginFromScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  Map<String, String> formData = {};
+
   void _onSubmitTap() {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
+        print(formData);
       }
     }
   }
@@ -54,7 +57,11 @@ class _LoginFromScreenState extends State<LoginFromScreen> {
 
                   return null; // 올바른 입력인 경우
                 },
-                onSaved: (newValue) => print(newValue),
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData["email"] = newValue;
+                  }
+                },
               ),
               Gaps.v16,
               TextFormField(
@@ -73,7 +80,11 @@ class _LoginFromScreenState extends State<LoginFromScreen> {
 
                   return null; // 올바른 입력인 경우
                 },
-                onSaved: (newValue) => print(newValue),
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData["password"] = newValue;
+                  }
+                },
               ),
               Gaps.v28,
               GestureDetector(
